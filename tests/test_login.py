621a01dc0244fc3ff924576397c1bede44bc4859
Leman-Quinn <PabloN.A@hotmail.com>
2026-06-09 02:39:37 -0300

    test_login.py: se implemento uso de archivo csv para credenciales

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,7 @@
 from pages.login_page import LogingPage
 from pages.inventory_page import InventoryPage
+from utils.datos import leer_csv_login
+import pytest
 import logging
 
 ####################################################################################################
@@ -13,16 +15,23 @@ import logging
 ####################################################################################################
 
 
-def test_login_exitoso_credenciales_validas(driver, credenciales_validas):
+@pytest.mark.parametrize(
+    "usuario, clave, debe_funcionar", leer_csv_login("datos/login.csv")
+)
+def test_login(driver, usuario, clave, debe_funcionar):
     logger = logging.getLogger(__name__)
 
     login_page = LogingPage(driver)
-    login_page.abrir().login_completo(*credenciales_validas)
+    login_page.abrir().login_completo(usuario, clave)
 
     inventory_page = InventoryPage(driver)
-    main_title = inventory_page.obtener_titulo()
 
-    logger.info(f"Título principal esperado: Swag Labs")
-    logger.info(f"Título principal encontrado: {main_title}")
+    if debe_funcionar:
+        main_title = inventory_page.obtener_titulo()
 
-    assert main_title == "Swag Labs"
+        logger.info(f"Título principal esperado: Swag Labs")
+        logger.info(f"Título principal encontrado: {main_title}")
+
+        assert main_title == "Swag Labs"
+    else:
+        assert login_page.obtener_mensaje_error()
